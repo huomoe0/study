426e03d35b2a2915eada7ccfd3c05fe47a522b40
baiji <sijielove1@qq.com>
2021-09-19 13:26:48 +0800

    测试代码

---
+++ code/2021_9_19/2021_9_19/test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int num = 10;
+int main()
+{
+	//int num = 1;
+	//printf("num = %d\n", num);
+	//return 0;
+
+
+
+	//int a = 0,b = 0;
+	//int sum = a + b;
+	//printf("请输入求和的两个数\n");
+	//scanf("%d%d", &a,&b);
+	//printf("和 = %d", sum);
+
+
+	/*puts("bit");*/
+
+
+	//char arr[] = { 'b','i','t','\0'};
+	//printf("%d\n", strlen(arr));   //strlen()  求字符串长度
+
+
+	int a = 2;
+	a += 2;
+	printf("%d", a);
+
+
+	return 0;
+}
