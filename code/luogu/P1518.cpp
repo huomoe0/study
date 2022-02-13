a844c01c3cb53581575f452a58f1255c1964fc87
baiji <sijielove1@qq.com>
2022-02-13 10:49:18 +0800

    1

--- code/luogu/P1518.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-typedef long long ll;
-using namespace std;
-int main()
-{
-    ios::sync_with_stdio(false);
-	
-}
--- code/项目/s/s/test.cpp
+++ code/项目/s/s/test.cpp
@@ -1,45 +1,24 @@
-#include <bits/stdc++.h>
-typedef long long ll;
+#include <iostream>
 using namespace std;
-const int mod = 1e9 + 7;
-int gua[5000];
+long long ans = 0;
+void h(int n, char a, char b, char c)
+{
+	if (n == 1)
+	{
+		cout << a << " -> " << c << "\n";
+		ans++;
+		return;
+	}
+	h(n - 1, a, c, b);
+	cout << a << " -> " << c << "\n";
+	ans++;
+	h(n - 1, b, a, c);
+}
 int main()
 {
-    ios::sync_with_stdio(false);
-    int n, m, t;
-    cin >> n >> m;
-    if (n == 0)
-    {
-        for (int i = 1; i <= m; i++)
-            cout << 0 << " ";
-    }
-    else
-    {
-        for (int i = 0; i < n; i++)
-        {
-            cin >> t;
-            gua[t]++;
-        }
-        for (int i = 1; i <= m; i++)
-        {
-            ll ans = 0;
-            int j = 0;
-            ans += gua[i] + gua[i * 2];
-            for (j = 1; j * 2 < i; j++)
-            {
-                int a = gua[j] + gua[j * 2];
-                gua[j]--, gua[j * 2]--;
-                int b = max(gua[i - j], 0) + max(gua[2 * (i - j)], 0);
-                gua[j]++, gua[j * 2]++;
-                ans += a * b;
-            }
-            if (j * 2 == i)
-            {
-                ans += gua[j] * max(0, gua[j] - 1);
-                ans += gua[j] * gua[j * 2];
-            }
-            cout << ans % mod << " ";
-        }
-    }
-    return 0;
+	int n;
+	cin >> n;
+	h(n,'A', 'B', 'C');
+	cout << ans << "\n";
+	return 0;
 }
