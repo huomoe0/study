df2d871d4f9308c795130c78f97ebe82430c9fd0
Lightty <sijielove1@qq.com>
2022-04-02 12:07:05 +0800

    1

--- code/datastucture/example/2.2 p(x) + f(x).c
+++ code/datastucture/example/2.2 p(x) + f(x).c
@@ -6,11 +6,11 @@ typedef struct node
     struct node *next;
 }node,*linklist;
 
-void initlinklist(linklist a)
+void initlinklist(linklist *a)
 {
-    a = (node*)malloc(sizeof(node));
-    if(!a)  exit(-1);
-    a->next = NULL,a->e = -1;
+    *a = (node*)malloc(sizeof(node));
+    if(!*a)  exit(-1);
+    (*a)->next = NULL,(*a)->e = -1;
 }
 
 void createpolyn(linklist a,int n) //small -> big
@@ -35,9 +35,9 @@ void createpolyn(linklist a,int n) //small -> big
                     free(q);
                 }
             }
-            else if(t->next)
+            else //p->e < t->next->e
             {
-                p->next = t->next->next;
+                p->next = t->next;
                 t->next = p;
             }
         }
@@ -51,7 +51,7 @@ void createpolyn(linklist a,int n) //small -> big
 
 void add(linklist a,linklist b) //a = a+b;
 {
-    node*pa = a,*pb = b,*p = a,*t;
+    node*pa = a->next,*pb = b->next,*p = a,*t;
     while(pb && pa)
     {
         if(pa->e == pb->e)
@@ -59,7 +59,7 @@ void add(linklist a,linklist b) //a = a+b;
             pa->c += pb->c;
             if(pa->c)
             {
-                p->next = pa,p = pa;
+                p->next = pa,p = pa,pa = pa->next;
                 t = pb,pb = pb->next;
                 free(t);
             }
@@ -81,34 +81,37 @@ void add(linklist a,linklist b) //a = a+b;
         }
     }
     p->next = pa?pa:pb;
-    free(pb);
+    free(b);
 }
 int main()
 {
     int n,m;
     scanf("%d %d",&n,&m);
     linklist a,b;
-    initlinklist(a);
-    initlinklist(b);
+    initlinklist(&a);
+    initlinklist(&b);
     createpolyn(a,n);
     createpolyn(b,m);
     add(a,b);
     node *p = a->next;
     int flag = 1;
-    while(!p)
+    while(p)
     {
         if(flag)
         {
             flag = 0;
-            printf("%d",p->c);
+            if(p->c == -1)  putchar('-');
+            else if(p->c != 1)  printf("%d",p->c);
         }
         else
         {
             if(p->c > 0)    putchar('+');
-            printf("%d",p->c);
+            if(p->c == -1)  putchar('-');
+            else if(p->c != 1)  printf("%d",p->c);
         }
         if(p->e > 0)    putchar('x');
         if(p->e > 1)    printf("^%d",p->e);
+        p = p->next;
     }
     return 0;
 }
